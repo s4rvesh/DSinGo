709e22f89af1b1c3d18c5b22201ea2932ddfc4fb
s4rvesh <sarvesh.upadhye@gmail.com>
2020-06-30 18:39:33 +0530

    Selection Sort

---
+++ Array/sorting/selection.go
@@ -0,0 +1,25 @@
+package sorting
+
+import "github.com/s4rvesh/DSinGo/Array/output"
+
+//SelectionSort on array
+func SelectionSort(array []int) []int {
+
+	for i := 0; i < len(array); i++ {
+
+		min := i
+
+		for j := i + 1; j < len(array); j++ {
+
+			if array[j] < array[min] {
+				min = j
+			}
+		}
+		array[i], array[min] = array[min], array[i]
+	}
+
+	output.Show(array)
+
+	return array
+
+}
