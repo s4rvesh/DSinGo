cb4716ce0766d45285141981d0bed464dbd5f522
s4rvesh <sarvesh.upadhye@gmail.com>
2020-06-30 18:38:56 +0530

    Insertion Sort

---
+++ Array/sorting/insertion.go
@@ -0,0 +1,25 @@
+package sorting
+
+import "github.com/s4rvesh/DSinGo/Array/output"
+
+//InsertionSort the array
+func InsertionSort(array []int) []int {
+
+	for i := 1; i < len(array); i++ {
+
+		key := array[i]
+		j := i - 1
+
+		for j >= 0 && array[j] > key {
+
+			array[j+1] = array[j]
+			j = j - 1
+
+		}
+		array[j+1] = key
+	}
+
+	output.Show(array)
+
+	return array
+}
