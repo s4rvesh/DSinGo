56fd0a344073f83ff874464a8fe222f18c7dc03b
s4rvesh <sarvesh.upadhye@gmail.com>
2020-06-30 18:41:36 +0530

    Sorting Done

--- Array/main.go
+++ Array/main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/s4rvesh/DSinGo/Array/input"
 	"github.com/s4rvesh/DSinGo/Array/output"
+	"github.com/s4rvesh/DSinGo/Array/searching"
 	"github.com/s4rvesh/DSinGo/Array/sorting"
 )
 
@@ -14,7 +15,18 @@ func main() {
 
 	array := input.Accept()
 	output.Show(array)
+	array = sorting.MergeSort(array)
 
-	sorting.Bubble(array)
+	//array = sorting.QuickSort(array)
+	//array = sorting.InsertionSort(array)
+	//array = sorting.Bubble(array)
+
+	var search int
+	fmt.Println("enter value to search in array")
+	fmt.Scanln(&search)
+
+	elementAt := searching.BinarySearch(array, search)
+
+	fmt.Printf("element found at %v", elementAt)
 
 }
